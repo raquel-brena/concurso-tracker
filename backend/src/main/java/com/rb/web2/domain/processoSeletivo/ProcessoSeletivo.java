6a2c9c5ddfa20f46bb991f54e7e11c1e9440d2b0
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-11-18 15:23:15 -0300

    feat: implement ProcessoSeletivo entity and clean up AuthenticationService imports

---
+++ backend/src/main/java/com/rb/web2/domain/processoSeletivo/ProcessoSeletivo.java
@@ -0,0 +1,26 @@
+package com.rb.web2.domain.processoSeletivo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@Getter 
+@Setter                                    
+@AllArgsConstructor               
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class ProcessoSeletivo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+}
