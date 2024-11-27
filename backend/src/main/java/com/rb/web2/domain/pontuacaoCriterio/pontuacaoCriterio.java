a0617593af1d363e4f06d6f196e5e77cce2a209a
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2024-11-27 14:01:16 -0300

    feat: add PontuacaoCriterio entity and establish relationships with CriterioAvaliacao and CandidateApplication

--- backend/src/main/java/com/rb/web2/domain/pontuacaoCriterio/pontuacaoCriterio.java
+++ backend/src/main/java/com/rb/web2/domain/pontuacaoCriterio/pontuacaoCriterio.java
@@ -2,7 +2,7 @@ package com.rb.web2.domain.criterioAvaliacao;
 
 import java.util.List;
 
-import com.rb.web2.domain.processoSeletivo.ProcessoSeletivo;
+import com.rb.web2.domain.criterioAvaliacao.CriterioAvaliacao;
 import com.rb.web2.domain.candidateApplication.CandidateApplication;
 
 import jakarta.persistence.Entity;
@@ -11,40 +11,44 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
-import jakarta.persistence.JoinTable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Table(name = "criterios")
+@Table(name = "pontuacao_criterio")
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class CriterioAvaliacao {
+public class pontuacaoCriterio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String nome;
-    private int peso;
+    private BigDecimal nota;
 
-    @ManyToMany(mappedBy = "criterios")
-    private List<ProcessoSeletivo> processos;
+    @ManyToOne
+    @JoinColumn(name = "criterio_avaliacao_id", nullable = false)
+    @MapsId // Mapeia a chave primária compartilhada
+    private CriterioAvaliacao criterio;  // Associando com a classe CriterioAvaliacao
 
-    @ManyToMany(mappedBy = "incricoes")
-    @JoinTable(name = "pontuacao_criterio", joinColumns = @JoinColumn(name = "criterio_avaliacao_id"), inverseJoinColumns = @JoinColumn(name = "candidate_application_id"))
-    private List<CandidateApplication> incricoes;
+    @ManyToOne
+    @JoinColumn(name = "candidate_application_id", nullable = false)
+    @MapsId // Mapeia a chave primária compartilhada
+    private CandidateApplication incricao;  // Agora vinculado ao CandidateApplication
 
     @Column(nullable = false)
     private boolean ativo = true; // É definido como true antes de ser salvo no banco de dados
