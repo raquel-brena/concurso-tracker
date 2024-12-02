c5f50a6adb3e63e671aa34bcc5cf2083c4db63fb
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2024-12-02 13:18:56 -0300

    fix: update request mapping in BuscarController to include API prefix

--- backend/src/main/java/com/rb/web2/controllers/BuscarController.java
+++ backend/src/main/java/com/rb/web2/controllers/BuscarController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@RequestMapping("/buscar")
+@RequestMapping("/api/buscar")
 public class BuscarController {
 
     @Autowired
