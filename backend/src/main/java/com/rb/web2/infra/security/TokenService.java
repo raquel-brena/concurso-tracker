f15be572c937b26a81617f73ebe9934345208bd7
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2024-12-17 07:27:07 -0300

    refactor: remove unused imports and methods in TokenService and VagaRepository for improved clarity

--- backend/src/main/java/com/rb/web2/infra/security/TokenService.java
+++ backend/src/main/java/com/rb/web2/infra/security/TokenService.java
@@ -1,19 +1,17 @@
 package com.rb.web2.infra.security;
 
-import com.rb.web2.domain.user.User;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.rb.web2.domain.user.User;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
