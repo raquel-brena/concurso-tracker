74e32dc21bc82c4c4501467df60308015711a66b
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2024-12-18 10:29:28 -0300

    feat: rename table from 'formacoes' to 'cargos' and clean up imports in Cargo entity

--- backend/src/main/java/com/rb/web2/domain/cargo/Cargo.java
+++ backend/src/main/java/com/rb/web2/domain/cargo/Cargo.java
@@ -1,39 +1,37 @@
 package com.rb.web2.domain.cargo;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 @Entity
-@Table(name = "formacoes")
+@Table(name = "cargos")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Cargo {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String descricao;
     private float remuneracao;
-    
     private boolean temporario;
 
     @Column(nullable = false)
