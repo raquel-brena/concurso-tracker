a26e85c9414751eb0911c6948910079fe29658d4
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2024-12-20 11:29:47 -0300

    refactor: update VagasRequestDTO validation annotations; enhance VagaService to utilize AuthorizationUtil for permission checks

--- backend/src/main/java/com/rb/web2/domain/vaga/dto/VagasRequestDTO.java
+++ backend/src/main/java/com/rb/web2/domain/vaga/dto/VagasRequestDTO.java
@@ -1,21 +1,22 @@
 package com.rb.web2.domain.vaga.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record VagasRequestDTO(
     @NotBlank(message = "O campo 'processoSeletivo' não pode ser vazio.")
     String processoSeletivoId, 
 
-    @NotBlank(message = "O campo 'cargo' não pode ser vazio.")
+    @NotNull(message = "O campo 'cargo' não pode ser vazio.")
     Long cargoId, 
 
-    @NotBlank(message = "O campo 'quantidade' não pode ser vazio.") @Positive(message = "O campo 'quantidade' deve ser maior que zero.")
+    @NotNull(message = "O campo 'quantidade' não pode ser vazio.") @Positive(message = "O campo 'quantidade' deve ser maior que zero.")
     int quantidade,
 
     String descricao, 
 
-    @NotBlank(message = "O campo 'taxaInscricao' não pode ser vazio.")
+    @NotNull(message = "O campo 'taxaInscricao' não pode ser vazio.")
     float taxaInscricao
 ) {
 }
