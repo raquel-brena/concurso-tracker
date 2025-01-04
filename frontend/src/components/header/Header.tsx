ca64eb04135d335dad4847099caede7f88b4c4a1
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2025-01-03 23:35:57 -0300

    feat: enhance Header component to accept customizable title, subtitle, and description props

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,12 @@
 import { HomeIcon } from "@radix-ui/react-icons";
 
-export const Header = () => {
+interface HeaderProps {
+  title: string;
+  subtitle: string;
+  description: string;
+}
+
+export const Header = ({ title, subtitle, description }: HeaderProps) => {
   return (
     <div className="flex flex-col w-full h-1/4 px-10 pt-2 space-y-4">
       <div className="flex text-center items-center text-sm select-none">
@@ -9,14 +15,9 @@ export const Header = () => {
           {">"} Editais {">"} Gerenciar processos seletivos
         </p>
       </div>
-      <p className="text-2xl text-red-500 font-medium">
-        Gerenciar processos seletivos
-      </p>
-      <p>
-        Gerencie todos os concursos cadastrados no sistema, você pode
-        visualizar, editar e organizar as informações de cada concurso.
-      </p>
-      <p className="font-semibold pt-3">GERENCIAR NOTÍCIAS</p>
+      <p className="text-2xl text-red-500 font-medium">{title}</p>
+      <p>{description}</p>
+      <p className="font-semibold pt-3 uppercase">{subtitle}</p>
     </div>
   );
 };
